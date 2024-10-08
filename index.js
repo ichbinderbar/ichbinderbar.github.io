f2fb6330371654010da3c2e8412f472832f7abdd
ichbinderbar <idrovel@gmail.com>
2024-10-08 19:12:10 +0100

    delete animation object to ensure safari compatibility

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const track = document.getElementById("image-track");
 
-// initial track state: a -50 offset on both values centers the track slider and the background images
-// to create an optical illusion of looking out a window
 track.dataset.percentage = "-50";
 track.dataset.prevPercentage = "-50";
 
 track.style.transform = `translate(-50%, -50%)`;
 
 const handleOnDown = (e) => {
-  track.dataset.mouseDownAt = e.clientX || e.touches[0].clientX; // handle both mouse and touch
+  track.dataset.mouseDownAt = e.clientX || e.touches[0].clientX;
   e.preventDefault();
 };
 
@@ -20,7 +18,7 @@ const handleOnUp = () => {
 const handleOnMove = (e) => {
   if (track.dataset.mouseDownAt === "0") return;
 
-  const clientX = e.clientX || e.touches[0].clientX; // handle both mouse and touch
+  const clientX = e.clientX || e.touches[0].clientX;
   const mouseDelta = parseFloat(track.dataset.mouseDownAt) - clientX,
     maxDelta = window.innerWidth / 2;
 
@@ -31,20 +29,10 @@ const handleOnMove = (e) => {
 
   track.dataset.percentage = nextPercentage;
 
-  track.animate(
-    {
-      transform: `translate(${nextPercentage}%, -50%)`,
-    },
-    { duration: 1200, fill: "forwards" }
-  );
+  track.style.transform = `translate(${nextPercentage}%, -50%)`;
 
   for (const image of track.getElementsByClassName("image")) {
-    image.animate(
-      {
-        objectPosition: `${100 + nextPercentage}% center`,
-      },
-      { duration: 1200, fill: "forwards" }
-    );
+    image.style.objectPosition = `${100 + nextPercentage}% center`;
   }
 };
 
@@ -52,38 +40,26 @@ const handleOnScroll = (e) => {
   const scrollDelta = e.deltaY || e.wheelDelta;
   const maxDelta = window.innerWidth / 2;
 
-  const percentage = (scrollDelta / maxDelta) * 25, // scroll sensitivity value
+  const percentage = (scrollDelta / maxDelta) * 25,
     nextPercentageUnconstrained =
       parseFloat(track.dataset.prevPercentage) + percentage,
     nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
 
   track.dataset.percentage = nextPercentage;
 
-  track.animate(
-    {
-      transform: `translate(${nextPercentage}%, -50%)`,
-    },
-    { duration: 1200, fill: "forwards" }
-  );
+  track.style.transform = `translate(${nextPercentage}%, -50%)`;
 
   for (const image of track.getElementsByClassName("image")) {
-    image.animate(
-      {
-        objectPosition: `${100 + nextPercentage}% center`,
-      },
-      { duration: 1200, fill: "forwards" }
-    );
+    image.style.objectPosition = `${100 + nextPercentage}% center`;
   }
 
   track.dataset.prevPercentage = nextPercentage;
 };
 
-// mouse events
 window.onmousedown = (e) => handleOnDown(e);
 window.onmouseup = (e) => handleOnUp(e);
 window.onmousemove = (e) => handleOnMove(e);
 
-// touch events with non-passive listener
 window.addEventListener("touchstart", (e) => handleOnDown(e), {
   passive: false,
 });
@@ -92,10 +68,9 @@ window.addEventListener(
   "touchmove",
   (e) => {
     handleOnMove(e);
-    e.preventDefault(); // prevent scrolling while swiping
+    e.preventDefault();
   },
   { passive: false }
 );
 
-// scroll event
 window.onwheel = (e) => handleOnScroll(e);
